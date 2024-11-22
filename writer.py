39f5132fd6dbdf8462c02b6bb6802c2ea18cb794
Arpan Bhandari <itsmearpan1@gmail.com>
2024-11-21 23:22:49 -0600

    Made writer

--- writer.py
+++ writer.py
@@ -1,7 +1,37 @@
 from webpage import Webpage
+import os
+import tldextract
+import secrets
+import time
+from pathlib import Path
+import json
+from datetime import datetime
 
 
 class File:
     @staticmethod
     def write(data: "Webpage"):
-        pass
+        try:
+            root_folder: str = os.getcwd()
+            folder: str = tldextract.extract(data.get_values()[0])
+            if (not os.path.exists(folder)):
+                os.mkdir(folder)
+            while True:
+                filename: str = File().generate_filename()
+                filepath = Path(root_folder).joinpath(
+                    "data", datetime.now().date, folder, filename)
+                if (not filepath.exists()):
+                    break
+            json_data = {
+                "url": f"{data.get_values()[0]}",
+                "data": f"{data.get_values()[1]}"
+            }
+            with open(filepath, "w") as file:
+                json.dump(json_data, file, indent=4)
+        except Exception as e:
+            print(e)
+
+    def generate_filename() -> str:
+        timestamp = int(time.time())
+        random_string = secrets.token_hex(10)
+        return f"{timestamp}_{random_string}.json"
