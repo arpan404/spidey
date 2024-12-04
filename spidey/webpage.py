2a0b9839743c8e6db5ddd673e4fc9deeb8d7949d
Arpan Bhandari <itsmearpan1@gmail.com>
2024-12-04 02:20:00 -0600

    #fix: Fixed the set data function

--- spidey/webpage.py
+++ spidey/webpage.py
@@ -1,17 +1,16 @@
-from typing import List, Dict, Tuple
+from typing import List, Set, Tuple
 
 
 class Webpage:
     def __init__(self, current_url: str) -> None:
         self.__current_url = current_url
         self.__page_html_data = None
-        self.__files_url = []
+        self.__files_url = set()
 
-    def set_data(self, data: Dict) -> 'Webpage':
-        self.__page_html_data = data.get('page_html')
-        self.__files_url = data.get('files_url', [])
+    def set_data(self, page_html_data: str, files_url: Set[str] = set()) -> 'Webpage':
+        self.__page_html_data = page_html_data
+        self.__files_url = files_url
         return self
 
     def get_values(self) -> Tuple[str, str, List[str]]:
         return self.__current_url, self.__page_html_data, self.__files_url
-
